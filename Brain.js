ec6d2190cbef50bba59145b782a87a2d06889215
ShinjiEbi <155539508+ShinjiEbi@users.noreply.github.com>
2026-03-06 00:52:03 +0100

    Update Brain.js

--- Brain.js
+++ Brain.js
@@ -1,106 +1,101 @@
-class Brain {
+/* =====================================================
+   EVOLUTION BRAIN
+===================================================== */
 
-    constructor(inputSize = 10, hiddenSize = 32, outputSize = 8) {
+class Brain{
 
-        this.inputSize = inputSize
-        this.hiddenSize = hiddenSize
-        this.outputSize = outputSize
+constructor(inputSize=7,hiddenSize=16,outputSize=2){
 
-        this.w1 = this.randomMatrix(hiddenSize, inputSize)
-        this.b1 = this.randomArray(hiddenSize)
+this.inputSize=inputSize
+this.hiddenSize=hiddenSize
+this.outputSize=outputSize
 
-        this.w2 = this.randomMatrix(outputSize, hiddenSize)
-        this.b2 = this.randomArray(outputSize)
-    }
+this.w1=this.randomMatrix(hiddenSize,inputSize)
+this.b1=this.randomArray(hiddenSize)
 
-    think(inputs) {
+this.w2=this.randomMatrix(outputSize,hiddenSize)
+this.b2=this.randomArray(outputSize)
+}
 
-        let hidden = []
+think(inputs){
 
-        for (let i = 0; i < this.hiddenSize; i++) {
+let hidden=[]
 
-            let sum = this.b1[i]
+for(let i=0;i<this.hiddenSize;i++){
 
-            for (let j = 0; j < this.inputSize; j++) {
-                sum += (inputs[j] || 0) * this.w1[i][j]
-            }
+let sum=this.b1[i]
 
-            hidden[i] = Math.tanh(sum)
-        }
+for(let j=0;j<this.inputSize;j++){
+sum+=(inputs[j]||0)*this.w1[i][j]
+}
 
-        let output = []
+hidden[i]=Math.tanh(sum)
+}
 
-        for (let i = 0; i < this.outputSize; i++) {
+let output=[]
 
-            let sum = this.b2[i]
+for(let i=0;i<this.outputSize;i++){
 
-            for (let j = 0; j < this.hiddenSize; j++) {
-                sum += hidden[j] * this.w2[i][j]
-            }
+let sum=this.b2[i]
 
-            output[i] = Math.tanh(sum)
-        }
+for(let j=0;j<this.hiddenSize;j++){
+sum+=hidden[j]*this.w2[i][j]
+}
 
-        return output
-    }
+output[i]=Math.tanh(sum)
+}
 
-    mutate(rate = 0.08) {
-        this.mutateMatrix(this.w1, rate)
-        this.mutateMatrix(this.w2, rate)
-        this.mutateArray(this.b1, rate)
-        this.mutateArray(this.b2, rate)
-    }
+return output
+}
 
-    clone() {
+clone(){
 
-        let clone = new Brain(this.inputSize, this.hiddenSize, this.outputSize)
+let b=new Brain(this.inputSize,this.hiddenSize,this.outputSize)
 
-        clone.w1 = JSON.parse(JSON.stringify(this.w1))
-        clone.w2 = JSON.parse(JSON.stringify(this.w2))
-        clone.b1 = [...this.b1]
-        clone.b2 = [...this.b2]
+b.w1=JSON.parse(JSON.stringify(this.w1))
+b.w2=JSON.parse(JSON.stringify(this.w2))
+b.b1=[...this.b1]
+b.b2=[...this.b2]
 
-        return clone
-    }
+return b
+}
 
-    randomMatrix(rows, cols) {
+mutate(rate=0.1){
 
-        let m = []
+this.mutateMatrix(this.w1,rate)
+this.mutateMatrix(this.w2,rate)
+}
 
-        for (let i = 0; i < rows; i++) {
-            m[i] = []
-            for (let j = 0; j < cols; j++) {
-                m[i][j] = (Math.random() * 2 - 1) * 0.7
-            }
-        }
+randomMatrix(rows,cols){
 
-        return m
-    }
+let m=[]
 
-    randomArray(size) {
-        return Array.from({ length: size },
-            () => (Math.random() * 2 - 1) * 0.7
-        )
-    }
+for(let i=0;i<rows;i++){
+m[i]=[]
+for(let j=0;j<cols;j++){
+m[i][j]=(Math.random()*2-1)*0.5
+}
+}
 
-    mutateMatrix(matrix, rate) {
+return m
+}
 
-        for (let i = 0; i < matrix.length; i++) {
-            for (let j = 0; j < matrix[i].length; j++) {
+randomArray(size){
+return Array.from({length:size},()=> (Math.random()*2-1)*0.5)
+}
 
-                if (Math.random() < rate) {
-                    matrix[i][j] += (Math.random() * 2 - 1) * 0.4
-                }
-            }
-        }
-    }
+mutateMatrix(matrix,rate){
 
-    mutateArray(array, rate) {
+for(let i=0;i<matrix.length;i++){
+for(let j=0;j<matrix[i].length;j++){
 
-        for (let i = 0; i < array.length; i++) {
-            if (Math.random() < rate) {
-                array[i] += (Math.random() * 2 - 1) * 0.5
-            }
-        }
-    }
+if(Math.random()<rate){
+matrix[i][j]+=(Math.random()*2-1)*0.3
+}
+
+}
+}
+
+}
+
 }
