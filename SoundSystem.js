c7585c4e41e4d1ea01e9900235583cd25e1b0323
ShinjiEbi <155539508+ShinjiEbi@users.noreply.github.com>
2026-03-06 01:16:30 +0100

    Update SoundSystem.js

--- SoundSystem.js
+++ SoundSystem.js
@@ -20,12 +20,12 @@ if(!audioUnlocked || !this.audio) return
 let base=200
 
 if(type==="food") base=400
-if(type==="danger") base=120
+if(type==="danger") base=150
 if(type==="help") base=600
 
-let freq=base+Math.abs(strength)*300
-let duration=0.15
-let volume=0.3+Math.abs(strength)
+let freq=base+Math.abs(strength)*400
+let duration=0.1 + Math.abs(strength)*0.6
+let volume=0.2 + Math.abs(strength)
 
 let osc=this.audio.createOscillator()
 let gain=this.audio.createGain()
@@ -38,7 +38,7 @@ osc.connect(gain)
 gain.connect(this.audio.destination)
 
 osc.start()
-osc.stop(this.audio.currentTime+duration)
+osc.stop(this.audio.currentTime + duration)
 
 }
 
